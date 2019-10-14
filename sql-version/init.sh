46c60628f502fe3c15b41de65cc17a7097741b6a
Hideki <hideki.alex@hotmail.com>
2019-10-14 01:45:45 -0300

    init.sh of sql-version api

--- sql-version/init.sh
+++ sql-version/init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+mvn -f starwars/ clean install
+
 docker run -e MYSQL_ROOT_PASSWORD=supersecret -e MYSQL_DATABASE=starwars -p 3305:3306 -d --name mysql mysql:5.7
 
+sleep 4.0
 
+java -jar starwars/target/starwars-0.0.1-SNAPSHOT.jar
--- sql-version/starwars/src/main/java/com/starwars/api/service/PlanetService.java
+++ sql-version/starwars/src/main/java/com/starwars/api/service/PlanetService.java
@@ -17,7 +17,6 @@ import com.starwars.api.repository.PlanetRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
